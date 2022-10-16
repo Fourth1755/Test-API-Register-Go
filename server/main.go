041e0e422b3f0c49d284e736555bad425b90fb53
Fourth1755 <fourthe.hero@gmail.com>
2022-10-16 23:56:18 +0700

    create

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Binding from JSON
+type Register struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Fullname string `json:"fullname" binding:"required"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/register", func(c *gin.Context) {
+		var json Register
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{
+			"register": json,
+		})
+	})
+	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
